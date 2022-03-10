3cce242c86ac1bc6cfc83a62f3fd66165731a4d5
Duru Chidi <duruchidi@gmail.com>
2022-03-10 23:01:04 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
-    }
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
