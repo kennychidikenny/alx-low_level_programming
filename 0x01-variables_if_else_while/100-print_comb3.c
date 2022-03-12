21f26ac08ec06eafc8afe33d6d73455c701b4373
Duru Chidi <duruchidi@gmail.com>
2022-03-12 04:24:21 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,21 +2,21 @@
 
 int main(void)
 {
-
-  for (int i = 0; i <= 9; i++)
-    {
-      for (int j = i + 1; j <= 9; j++)
+    
+	for (int i = 0; i <= 9; i++)
 	{
-	  putchar(i + '0');
-	  putchar(j + '0');
-	  if (((i * 10) + j) < 89)
+	    for (int j = i + 1; j <= 9; j++)
 	    {
-	      putchar(',');
-	      putchar(' ');
+	        putchar(i + '0');
+	        putchar(j + '0');
+	        if (((i * 10) + j) < 89)
+	        {
+	            putchar(',');
+                putchar(' ');
 
+	        }
 	    }
 	}
-    }
-  putchar('\n');
-  return 0;
+	putchar('\n');
+    return 0;
 }
