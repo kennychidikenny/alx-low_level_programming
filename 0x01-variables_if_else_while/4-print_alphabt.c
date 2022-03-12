7b2f1cd283339ecd043c5cb7445bf74ece60830b
Duru Chidi <duruchidi@gmail.com>
2022-03-12 03:48:59 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,48 +2,48 @@
 
 int main(void)
 {
-  char ch = 'a';
-  while(ch <= 'z')
-    {
-      if (ch != 'e' && ch != 'q')
+    char ch = 'a';
+	while(ch <= 'z')
 	{
-	  putchar(ch);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
 	}
-      ch++;
-    }
-  putchar('\n');
+	putchar('\n');
 
-  return 0;
+    return 0;
 }
 
 #include <stdio.h>
 
 int main(void)
 {
-
-  for (char ch = 'a'; ch <= 'z'; ch++)
-    {
-      if (ch != 'e' && ch != 'q')
+    
+	for (char ch = 'a'; ch <= 'z'; ch++)
 	{
-	  putchar(ch);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
 	}
-    }
-  putchar('\n');
+	putchar('\n');
 
-  return 0;
+    return 0;
 }
 
 int main(void)
 {
+    
+	for (char ch ='a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		    ch++;
+		putchar(ch);
+		
+	}
+	putchar('\n');
 
-  for (char ch ='a'; ch <= 'z'; ch++)
-    {
-      if (ch == 'e' || ch == 'q')
-	ch++;
-      putchar(ch);
-
-    }
-  putchar('\n');
-
-  return 0;
+    return 0;
 }
