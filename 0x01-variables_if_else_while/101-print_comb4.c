910131b37a5ab5d20d1fd034c4595e53859c3d32
Duru Chidi <duruchidi@gmail.com>
2022-03-12 04:32:49 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,26 +2,26 @@
 
 int main()
 {
-  int a, b, c;
-
-  for (int i = 0; i <= 9; i++)
-    {
-      for (int j = i + 1; j <= 9; j++)
+    int a, b, c;
+    
+	for (int i = 0; i <= 9; i++)
 	{
-	  for (int k = j + 1; k <= 9; k++)
+	    for (int j = i + 1; j <= 9; j++)
 	    {
-	      putchar(i + '0');
-	      putchar(j + '0');
-	      putchar(k + '0');
-	      if (((i * 100) + (j * 10) + k) < 789)
-		{
-		  putchar(',');
-		  putchar(' ');
+	        for (int k = j + 1; k <= 9; k++)
+	        {
+	            putchar(i + '0');
+	            putchar(j + '0');
+	            putchar(k + '0');
+	            if (((i * 100) + (j * 10) + k) < 789)
+	            {
+	                putchar(',');
+                    putchar(' ');
 
-		}
+	            }
+	        }
 	    }
 	}
-    }
-  putchar('\n');
-  return 0;
+	putchar('\n');
+    return 0;
 }
